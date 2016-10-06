4bc2231f18786cc6d9cb01aa6ae2eeb29a533818
annnufan <annnufan@gmail.com>
2016-10-06 14:34:41 +0300

    1.Insert OK Iterators better Find bug in Erase

--- persistent_set.cpp
+++ persistent_set.cpp
@@ -127,15 +127,16 @@ persistent_set::iterator persistent_set::end() const {
  // Удаление элемента.
 // Инвалидирует все итераторы, принадлежащие persistent_set'у this, включая end().
 void persistent_set::erase(iterator elem) {
+    assert(elem.value != nullptr);
     invalidate_all_iterators();
     root = erase_value(elem.version_root, elem.value);
 }
 
 persistent_set::node* persistent_set::erase_value(node* v, node* erase_node) {
-    if (v->get_value() < erase_node->get_value()) {
+    if (v->get_value() > erase_node->get_value()) {
         return new node(v->get_value(), erase_value(v->left, erase_node), v->right);
     }
-    if (v->get_value() > erase_node->get_value()) {
+    if (v->get_value() < erase_node->get_value()) {
         return new node(v->get_value(), v->left, erase_value(v->right, erase_node));
     }
     if (v->right == nullptr) {
@@ -231,12 +232,11 @@ persistent_set::node* persistent_set::node::get_max() {
 // Инкремент невалидного итератора неопределен.
 persistent_set::iterator& persistent_set::iterator::operator++() {
     if (value->right != nullptr) {
-        //std::cerr << "has right subtree" << std::endl;
         value = value->right->get_min();
         return *this;
     }
     node* r = version_root;
-    std::vector<*node> parent;
+    std::vector<node*> parent;
     while (r->right != value && r->left != value) {
         parent.push_back(r);
         if (r->get_value() < value->get_value()) {
@@ -245,13 +245,12 @@ persistent_set::iterator& persistent_set::iterator::operator++() {
             r = r->left;
         }
     }
-    if (r->left == value) {
-        value = r;
-    } else {
-        while (parent.back()->left == value)
-            parent.pop_back();
+    parent.push_back(r);
+    while (parent.back()->right == value) {
         value = parent.back();
+        parent.pop_back();
     }
+    value = parent.back();
     return *this;
 }
 persistent_set::iterator persistent_set::iterator::operator++(int) {
@@ -270,21 +269,22 @@ persistent_set::iterator& persistent_set::iterator::operator--() {
         value = value->left->get_max();
         return *this;
     }
-    node* r = version_root, *last_root = version_root;
+    node* r = version_root;
+    std::vector<node*> parent;
     while (r->right != value && r->left != value) {
+        parent.push_back(r);
         if (r->get_value() < value->get_value()) {
-            last_root = r;
             r = r->right;
         } else {
-            last_root = r;
             r = r->left;
         }
     }
-    if (r -> right == value) {
-        value = r;
-    } else {
-        value = last_root;
+    parent.push_back(r);
+    while (parent.back()->left == value) {
+        value = parent.back();
+        parent.pop_back();
     }
+    value = parent.back();
     return *this;
 }
 persistent_set::iterator persistent_set::iterator::operator--(int) {
@@ -298,6 +298,7 @@ persistent_set::iterator persistent_set::iterator::operator--(int) {
 
 void persistent_set::print() {
     node::print_node(root);
+    std::cout << std::endl;
 }
 
 void persistent_set::node::print_node(node* v) {
@@ -311,20 +312,25 @@ void persistent_set::node::print_node(node* v) {
 }
 
 int main() {
-    int a[7] = {2, 5, 3, 7, 1, 9, 1};
+    int a[6] = {2, 5, 3, 7, 1, 9};
     persistent_set me;
-    //std::cerr << me.root->left->get_value();
     for (int i : a) {
         me.insert(i);
     }
     me.print();
-    persistent_set::iterator ex = me.begin();
-    std::cout << *(ex) << std::endl;
-    while (ex != me.end()) {
-        std::cout << *(++ex) << std::endl;
+    persistent_set::iterator ex = me.end();
+    /*std::cout << *(ex) << std::endl;
+    while (ex != me.begin()) {
+        std::cout << *(ex--) << std::endl;
+    }*/
+    for (int i : a) {
+        me.erase(me.find(i));
+        me.print();
     }
     /*for (int i : a) {
-        me.erase(me.find(i));
+        ex = me.find(i);
+        std::cout << *ex << ' ';
+        me.erase(ex);
         ex = me.begin();
         std::cout << *ex << std::endl;
     }*/
